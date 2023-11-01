eebb0f20b02e2d3234d0ef8c46162c184e7a955d
Diego Zeiter <diegozeiter7@gmail.com>
2023-11-01 09:35:22 +0100

    Implemented Boxplot for DICE and Hausdorff per label

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -11,23 +11,63 @@ def main():
     current_directory = os.getcwd()
     target_directory = os.path.join(current_directory, 'mia-result')
 
-    folder_name = '2023-10-31-14-59-59' # change here
+    folder_name = '2023-11-01-08-41-52'  # Change here!!!!!!!!!
     file_name = 'results.csv'
 
     file_path = os.path.join(target_directory, folder_name, file_name)
     print(file_path)
 
-    data = pd.read_csv(filepath)
+    # Read the data from the results.csv file
+    data = pd.read_csv(file_path, sep=";")
 
     # to continue we need to be able to run the pipeline
 
-    # todo: read the data into a list
-    # todo: plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
+    # (DONE): read the data into a list
+    # (DONE) plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
     #  in a boxplot
+
+    # Set the figure size
+    plt.figure(figsize=(10, 6))
+
+    # Use boxplot function to create boxplot for DICE
+    plt.boxplot([data[data["LABEL"] == label]["DICE"]
+                 for label in data["LABEL"].unique()],
+                labels=data["LABEL"].unique())
+
+    # Adding title x and y label:
+    plt.title("Dice Coefficients by Label")
+    plt.xlabel("Label")
+    plt.ylabel("Dice Coefficients")
+
+    # Save the boxplot as png file
+    plt.savefig("dice_boxplot.png")
+
+    plt.show()
+
+    # Now the same thing for the Hausdorff => uncomment this as soon as Hausdorff has been implemented and then you get
+    # the boxplot
+
+    # # Set the figure size
+    # plt.figure(figsize=(10, 6))
+    #
+    # # Use boxplot function to create boxplot for HAUSDORFF
+    # plt.boxplot([data[data["LABEL"] == label]["HAUSDORFF"]
+    #              for label in data["LABEL"].unique()],
+    #             labels=data["LABEL"].unique())
+    #
+    # # Adding title x and y label:
+    # plt.title("Hausdorff Coefficients by Label")
+    # plt.xlabel("Label")
+    # plt.ylabel("Hausdorff Coefficients")
+    #
+    # plt.savefig("hausdorff_boxplot.png")
+    #
+    # plt.show()
+
+    # labels = ["Amygdala", "GreyMatter", "Hippocampus", "Thalamus", "WhiteMatter"]
 
     # alternative: instead of manually loading/reading the csv file you could also use the pandas package
     # but you will need to install it first ('pip install pandas') and import it to this file ('import pandas as pd')
-
 
     # pass  # pass is just a placeholder if there is no other code
 
