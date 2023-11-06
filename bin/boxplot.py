e5881d8e1008532eacee4895f68c88cd4e10716c
Diego Zeiter <diegozeiter7@gmail.com>
2023-11-06 20:43:40 +0100

    Boxplots are now beautiful

--- bin/boxplot.py
+++ bin/boxplot.py
@@ -5,67 +5,142 @@ import pandas as pd
 
 
 def main():
-    # to-do: load the "results.csv" file from the mia-results directory
 
     # Get the current working directory
     current_directory = os.getcwd()
     target_directory = os.path.join(current_directory, 'mia-result')
 
-    folder_name = '2023-11-06-09-20-08'  # Change here!!!!!!!!!
+    folder_name_baseline = '2023-11-06-19-15-49'  # Change here!!!!!!!!!
+    folder_name_features = '2023-11-06-18-05-53'  # Change here!!!!!!!!!
     file_name = 'results.csv'
 
-    file_path = os.path.join(target_directory, folder_name, file_name)
-    print(file_path)
+    file_path_baseline = os.path.join(target_directory, folder_name_baseline, file_name)
+    print(file_path_baseline)
+
+    file_path_features = os.path.join(target_directory, folder_name_features, file_name)
+    print(file_path_features)
 
     # Read the data from the results.csv file
-    data = pd.read_csv(file_path, sep=";")
+    baseline = pd.read_csv(file_path_baseline, sep=";")
+    features = pd.read_csv(file_path_features, sep=";")
 
-    # to continue we need to be able to run the pipeline
+    ############ DICE PLOTTING
+    # Create a list of labels
+    labels = baseline["LABEL"].unique()
 
-    # (DONE): read the data into a list
-    # (DONE) plot the Dice coefficients per label (i.e. white matter, gray matter, hippocampus, amygdala, thalamus)
-    #  in a boxplot
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(12, 6))
 
-    # Set the figure size
-    plt.figure(figsize=(10, 6))
+    # Define a color for the baseline and features boxplots
+    baseline_color = 'blue'
+    features_color = 'green'
 
-    # Use boxplot function to create boxplot for DICE
-    plt.boxplot([data[data["LABEL"] == label]["DICE"]
-                 for label in data["LABEL"].unique()],
-                labels=data["LABEL"].unique())
+    # Initialize a position variable
+    position = 1
 
-    # Adding title x and y label:
+    # Create lists to store data for baseline and features
+    baseline_data = []
+    features_data = []
+
+    # Adjust the positions for labels and boxplots
+    label_positions = range(1, len(labels) * 3, 3)
+    baseline_positions = [pos - 0.25 for pos in label_positions]
+    features_positions = [pos + 0.25 for pos in label_positions]
+
+    # Iterate over labels and extract DICE values for baseline and features
+    for label in labels:
+        baseline_data.append(baseline[baseline["LABEL"] == label]["DICE"])
+        features_data.append(features[features["LABEL"] == label]["DICE"])
+
+    # Create boxplots for baseline data
+    boxplot1 = ax.boxplot(baseline_data, positions=baseline_positions, widths=0.4, patch_artist=True)
+    for box in boxplot1['boxes']:
+        box.set(facecolor=baseline_color)
+
+    # Create boxplots for features data
+    boxplot2 = ax.boxplot(features_data, positions=features_positions, widths=0.4, patch_artist=True)
+    for box in boxplot2['boxes']:
+        box.set(facecolor=features_color)
+
+    # Set y-axis limits
+    ax.set_ylim(0.4, 0.85)
+
+    # Add x-axis labels for each label
+    ax.set_xticks(label_positions)
+    ax.set_xticklabels(labels)
+
+    # Adding title and labels
     plt.title("Dice Coefficients by Label")
     plt.xlabel("Label")
     plt.ylabel("Dice Coefficients")
 
-    # Set y-axis limits
-    plt.ylim(0.4, 0.85)
+    # Create a legend
+    legend_labels = ['Baseline', 'Features']
+    handles = [plt.Rectangle((0, 0), 1, 1, color=baseline_color), plt.Rectangle((0, 0), 1, 1, color=features_color)]
+    ax.legend(handles, legend_labels)
 
-    # Save the boxplot as png file
+    # Save the boxplot as a PNG file
     plt.savefig("dice_boxplot.png")
 
     plt.show()
 
-    # Now the same thing for the Hausdorff => uncomment this as soon as Hausdorff has been implemented and then you get
-    # the boxplot
 
-    # Set the figure size
-    plt.figure(figsize=(10, 6))
+    ############ HAUSDORFF PLOTTING
+    # Create a list of labels
+    labels = baseline["LABEL"].unique()
 
-    # Use boxplot function to create boxplot for HAUSDORFF
-    plt.boxplot([data[data["LABEL"] == label]["HDRFDST"]
-                 for label in data["LABEL"].unique()],
-                labels=data["LABEL"].unique())
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(12, 6))
 
-    # Adding title x and y label:
-    plt.title("Hausdorff Coefficients by Label")
+    # Define a color for the baseline and features boxplots
+    baseline_color = 'blue'
+    features_color = 'green'
+
+    # Initialize a position variable
+    position = 1
+
+    # Create lists to store data for baseline and features
+    baseline_data = []
+    features_data = []
+
+    # Adjust the positions for labels and boxplots
+    label_positions = range(1, len(labels) * 3, 3)
+    baseline_positions = [pos - 0.25 for pos in label_positions]
+    features_positions = [pos + 0.25 for pos in label_positions]
+
+    # Iterate over labels and extract DICE values for baseline and features
+    for label in labels:
+        baseline_data.append(baseline[baseline["LABEL"] == label]["HDRFDST"])
+        features_data.append(features[features["LABEL"] == label]["HDRFDST"])
+
+    # Create boxplots for baseline data
+    boxplot1 = ax.boxplot(baseline_data, positions=baseline_positions, widths=0.4, patch_artist=True)
+    for box in boxplot1['boxes']:
+        box.set(facecolor=baseline_color)
+
+    # Create boxplots for features data
+    boxplot2 = ax.boxplot(features_data, positions=features_positions, widths=0.4, patch_artist=True)
+    for box in boxplot2['boxes']:
+        box.set(facecolor=features_color)
+
+    # Set y-axis limits
+    ax.set_ylim(1, 17.5)
+
+    # Add x-axis labels for each label
+    ax.set_xticks(label_positions)
+    ax.set_xticklabels(labels)
+
+    # Adding title and labels
+    plt.title("Hausdorff Distance by Label")
     plt.xlabel("Label")
     plt.ylabel("Hausdorff Coefficients")
 
-    # Set y-axis limits
-    plt.ylim(0, 18)
+    # Create a legend
+    legend_labels = ['Baseline', 'Features']
+    handles = [plt.Rectangle((0, 0), 1, 1, color=baseline_color), plt.Rectangle((0, 0), 1, 1, color=features_color)]
+    ax.legend(handles, legend_labels)
 
+    # Save the boxplot as a PNG file
     plt.savefig("hausdorff_boxplot.png")
 
     plt.show()
